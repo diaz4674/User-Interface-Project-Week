6810322da036d8f4ac0b23bc806f32999c7af3ee
Miguel <diaz4674@gmail.com>
2019-02-18 20:07:04 -0800

    Finished Expanded Desktop Nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // JS goes here
+//Expanded Nav
+let expandedNav = document.querySelector(".navOptions");
+
+const toggleNav = () => {
+    expandedNav.classList.toggle("navOptions--open")
+}
+
+let hamburger = document.querySelector('.hamburger')
+
+hamburger.addEventListener('click', (event) => {
+    toggleNav(event);
+});
+
+//Closing Expanded Nav
+
+const toggleNavClose = () => {
+    expandedNav.classList.remove("navOptions--open")
+}
+
+let hamburgerClose = document.querySelector('.hamburgerClose')
+
+hamburgerClose.addEventListener('click', (event) => {
+    toggleNavClose(event);
+});
